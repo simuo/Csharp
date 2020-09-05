b7811326a9f7f2c71a338b0d37424da900363232
simuo <3286649448@qq.com>
2020-09-05 11:21:20 +0800

    add first Application HelloWorld

---
+++ 01HelloWorldApplication/HelloWorldApplication/HelloWorldApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("你好漂亮呀！@@");
+            Console.WriteLine("asjdaiso");
+            Console.WriteLine("asjdaiso");
+            Console.WriteLine("asjdaiso");
+            Console.WriteLine("asjdaiso");
+            Console.WriteLine("asjdaiso");
+        }
+    }
+}
