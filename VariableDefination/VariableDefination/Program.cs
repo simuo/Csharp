82bd9d3f99ac3c5745091119d6968752509b05aa
simuo <3286649448@qq.com>
2020-09-07 00:24:30 +0800

    add Variable

---
+++ VariableDefination/VariableDefination/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariableDefination
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            short a;
+            int b;
+            double c;
+            
+            //初始化
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("a={0}, b={1}, c={2}",a,b,c);
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input = {0}",input);
+        }
+    }
+}
