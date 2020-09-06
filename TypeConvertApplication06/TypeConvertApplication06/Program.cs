75e535858e058abe7e46fb3e0b9124b916839f3e
simuo <3286649448@qq.com>
2020-09-06 23:23:05 +0800

    ToString test

--- TypeConvertApplication06/TypeConvertApplication06/Program.cs
+++ TypeConvertApplication06/TypeConvertApplication06/Program.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 
-namespace TypeConvertApplication06
+// namespace TypeConvertApplication06
+// {
+//     internal class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             double t = 3.12334123123;
+//             // Console.WriteLine(sizeof(double));
+//             int i;
+//             
+//             //强制转换double为int
+//             i = (int) t;
+//             Console.WriteLine(i);
+//             Console.ReadKey();
+//         }
+//     }
+// }
+
+namespace TypeconvertionApplication
 {
-    internal class Program
+    class convertFunction
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            double t = 3.12334123123;
-            // Console.WriteLine(sizeof(double));
-            int i;
+            int i = 1;
+            float f = 3.2112123f;
+            double d = 2.112121212121212121;
+            bool b = true;
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
+            Console.WriteLine();
             
-            //强制转换double为int
-            i = (int) t;
-            Console.WriteLine(i);
-            Console.ReadKey();
         }
     }
 }
