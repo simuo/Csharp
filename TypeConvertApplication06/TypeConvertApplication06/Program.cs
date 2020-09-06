0feb5e6ecec1be1f3506f5f26922b4ddb0cb9a69
simuo <3286649448@qq.com>
2020-09-06 22:16:16 +0800

    add typeconvert111

---
+++ TypeConvertApplication06/TypeConvertApplication06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TypeConvertApplication06
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double t = 3.12334123123;
+            // Console.WriteLine(sizeof(double));
+            int i;
+            
+            //强制转换double为int
+            i = (int) t;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
