8be06f01c0368f46747289d3a1dd36c4777e8296
simuo <3286649448@qq.com>
2020-09-06 22:08:07 +0800

    add typeconvert

---
+++ GetDatatypeApplication/GetDatatypeApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+
+namespace GetDatatypeApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("size of int:{0}",sizeof(int));
+            Console.WriteLine("size of byte:{0}",sizeof(byte));
+            Console.WriteLine("size of float:{0}",sizeof(float));
+            Console.WriteLine("size of double:{0}",sizeof(double));
+            Console.WriteLine("size of long:{0}",sizeof(long));
+            Console.WriteLine("size of char:{0}",sizeof(char));
+            // Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle02/Rectangle02/Program.cs
@@ -0,0 +1,39 @@
+﻿﻿using System;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 10;
+            width = 4.2;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("length:{0}",length);
+            Console.WriteLine("width:{0}",width);
+            Console.WriteLine("Area=legth*width:{0}={1} x {2}",GetArea(),length,width);
+        }
+    }
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect=new Rectangle();
+            rect.Acceptdetails();
+            rect.display();
+            Console.ReadLine();
+        }
+    }
+}
