f3df017599afda37f00f6deb19e12670ca818c4a
simuo <3286649448@qq.com>
2020-09-08 08:15:43 +0800

    add reference

---
+++ Reference13/Reference13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reference13
+{ 
+    class Program
+    {
+        public void getValues(out int x,out int y)
+        {
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+        public static void Main(string[] args)
+        {
+            Program p =new Program();
+            int a, b;
+            p.getValues(out a,out b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            
+        }
+    }
+}
